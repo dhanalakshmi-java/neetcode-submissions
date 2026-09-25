696cd4bb4e831ea6c4bc5ece053422c2cd5adb35
Dhanalakshmi V <dhanalakshmivs9626@gmail.com>
2026-09-25 14:23:36 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<Character>[] row=new HashSet[9];
+        HashSet<Character>[] col=new HashSet[9];
+        HashSet<Character>[] box=new HashSet[9];
+        for(int i=0;i<9;i++)
+        {
+            row[i]=new HashSet<>();
+            col[i]=new HashSet<>();
+            box[i]=new HashSet<>();
+        }
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                char num=board[i][j];
+                if(num=='.')continue;
+                int boxn=(i/3)*3+(j/3);
+            if(row[i].contains(num)||col[j].contains(num)||box[boxn].contains(num))
+                return false; 
+                row[i].add(num);
+                col[j].add(num);
+                box[boxn].add(num); 
+            }
+        }
+        return true;
+
+
+        
+        
+    }
+}
